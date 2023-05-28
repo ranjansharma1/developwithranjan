ff3e70632157d043bd42c43059537c2ee1b6d9fe
RanjanSharma <ranjansharmadto@gmail.com>
2023-05-28 20:44:29 +0530

    Removing sticky feature from Footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <>
-      <footer className="text-blue-600 body-font lg:sticky lg:inset-x-0 lg:bottom-0" >
+      <footer className="text-blue-600 body-font " >
         <div className="bg-blue-950 " style={{backgroundColor: "rgba(0, 13, 101, 0.72)"}}>
           <div className="container mx-auto py-2  flex flex-wrap flex-col sm:flex-row">
           <div class="flex justify-center items-center">
